e641c52b7b117862133db90a70c79502218b20e7
Konstantin Muzyka <kosatkinms@mail.ru>
2021-11-29 23:38:48 +0300

    sliders refactoring

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,70 +1,70 @@
-const menus = document.querySelectorAll('.menu');
-const subMenus = document.querySelectorAll('.menu_sub');
+// const menus = document.querySelectorAll('.menu');
+// const subMenus = document.querySelectorAll('.menu_sub');
 
-const hangHandler = () => {
-    menus.forEach(menu => {
-        menu.querySelectorAll('.menu__link').forEach(link => {
-            link.onclick = clickLink.bind(link);
-        })
-    })
-}
-
-function clickLink() { 
-    let parent = this.closest('.menu__item');
-    let subMenu = parent.querySelector('.menu_sub');
-
-    if (subMenu) {
-        changeMenuClass(subMenu);
-        return false;
-    }
-}
-
-const changeMenuClass = (currentSubMenu) => {
-    subMenus.forEach(element => {
-      if (currentSubMenu !== element && element.classList.contains('menu_active')) {
-        element.classList.remove('menu_active');
-      }
-    });
-    currentSubMenu.classList.toggle('menu_active');
-  };
-
-  hangHandler();
-
-
-// class Menu {
-//     constructor(menuSelector) {
-//         this.menu = document.querySelector(menuSelector);
-//         this.links = this.menu.querySelectorAll('.menu__link');
-//         this.subMenus = this.menu.querySelectorAll('.menu_sub');
-
-//         this.links.forEach(link => {
-//             link.addEventListener('click', this.clickLink);
-//         });
-//     } 
-    
-//     clickLink = (link) => {
-//         let parent = link.target.closest('.menu__item');
-//         let subMenu = parent.querySelector('.menu_sub');
-        
-//         if (subMenu) {
-//             link.preventDefault();
-//             this.changeMenuClass(subMenu);
-//         }
-//     }
-    
-//     changeMenuClass = (currentSubMenu) => {
-//         this.subMenus.forEach(element => {
-//           if (currentSubMenu !== element && element.classList.contains('menu_active')) {
-//             element.classList.remove('menu_active');
-//           }
-//         });
-//         currentSubMenu.classList.toggle('menu_active');
-//     }    
+// const hangHandler = () => {
+//     menus.forEach(menu => {
+//         menu.querySelectorAll('.menu__link').forEach(link => {
+//             link.onclick = clickLink.bind(link);
+//         })
+//     })
 // }
 
+// function clickLink() { 
+//     let parent = this.closest('.menu__item');
+//     let subMenu = parent.querySelector('.menu_sub');
 
-// const firstMenu = new Menu('.menu_main');
-// const secondMenu = new Menu('.menu_additional');
+//     if (subMenu) {
+//         changeMenuClass(subMenu);
+//         return false;
+//     }
+// }
+
+// const changeMenuClass = (currentSubMenu) => {
+//     subMenus.forEach(element => {
+//       if (currentSubMenu !== element && element.classList.contains('menu_active')) {
+//         element.classList.remove('menu_active');
+//       }
+//     });
+//     currentSubMenu.classList.toggle('menu_active');
+//   };
+
+//   hangHandler();
+
+
+class Menu {
+    constructor(menuSelector) {
+        this.menu = document.querySelector(menuSelector);
+        this.links = this.menu.querySelectorAll('.menu__link');
+        this.subMenus = this.menu.querySelectorAll('.menu_sub');
+
+        this.links.forEach(link => {
+            link.addEventListener('click', this.clickLink);
+        });
+    } 
+    
+    clickLink = (link) => {
+        let parent = link.target.closest('.menu__item');
+        let subMenu = parent.querySelector('.menu_sub');
+        
+        if (subMenu) {
+            link.preventDefault();
+            this.changeMenuClass(subMenu);
+        }
+    }
+    
+    changeMenuClass = (currentSubMenu) => {
+        this.subMenus.forEach(element => {
+          if (currentSubMenu !== element && element.classList.contains('menu_active')) {
+            element.classList.remove('menu_active');
+          }
+        });
+        currentSubMenu.classList.toggle('menu_active');
+    }    
+}
+
+
+const firstMenu = new Menu('.menu_main');
+const secondMenu = new Menu('.menu_additional');
 
 
   
--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,84 +1,143 @@
-const images = [...document.querySelectorAll('.slider__item')];
-const arrows = document.querySelectorAll('.slider__arrow');
-const dots = [...document.querySelectorAll('.slider__dot')];
+const images = [...document.querySelectorAll(".slider__item")];
+const arrows = document.querySelectorAll(".slider__arrow");
+const dots = [...document.querySelectorAll(".slider__dot")];
 
-let itemIndex;
+// let itemIndex;
 
-// Поиск индекса активного слайда
+// // Поиск индекса активного слайда
 
-const findIndex = () => {
-    images.forEach(image => {
-        if (image.classList.contains('slider__item_active')) {
-            itemIndex = parseInt(images.indexOf(image), 10);
-        }
-    })
-}
+// const findItemIndex = () => {
+//     itemIndex = images.findIndex(image => image.classList.contains('slider__item_active'));
+// }
 
-// Регистрация обработчиков события на стрелки
+// // Регистрация обработчиков события на стрелки
 
-arrows.forEach(arrow => {
-    if (arrow.classList.contains('slider__arrow_prev')) {
-        arrow.onclick = () => {
-            changeImagePrev();
-            console.log(itemIndex)
-        }
-    } else if (arrow.classList.contains('slider__arrow_next')) {
-        arrow.onclick = () => {
-            changeImageNext();
-            console.log(itemIndex)
-        }
-    }
-})
+// arrows.forEach(arrow => {
+//     if (arrow.classList.contains('slider__arrow_prev')) {
+//         arrow.onclick = () => {
+//             changeImagePrev();
+//             console.log(itemIndex)
+//         }
+//     } else if (arrow.classList.contains('slider__arrow_next')) {
+//         arrow.onclick = () => {
+//             changeImageNext();
+//             console.log(itemIndex)
+//         }
+//     }
+// })
 
-// Регистрация обработчиков события на точки
+// // Регистрация обработчиков события на точки
 
-dots.forEach(dot => {
-    dot.onclick = () => {
-        resetImageClass();
+// dots.forEach(dot => {
+//     dot.onclick = () => {
+//         resetImageClass();
 
-        dot.classList.toggle('slider__dot_active');
-        images[dots.indexOf(dot)].classList.toggle('slider__item_active');
+//         dot.classList.toggle('slider__dot_active');
+//         images[dots.indexOf(dot)].classList.toggle('slider__item_active');
 
-        findIndex();
-    }
-})
+//         findItemIndex();
+//     }
+// })
 
-// Смена слайда на предыдущий
+// // Смена слайда на предыдущий
 
-const changeImagePrev = () => {
-    if (itemIndex > 0) {
-        itemIndex--;
-    } else {
-        itemIndex = images.length - 1;
-    }
+// const changeImagePrev = () => {
+//     if (itemIndex > 0) {
+//         itemIndex--;
+//     } else {
+//         itemIndex = images.length - 1;
+//     }
 
-    resetImageClass();
-    images[itemIndex].classList.toggle('slider__item_active');
-    dots[itemIndex].classList.toggle('slider__dot_active');
-}
+//     resetImageClass();
+//     images[itemIndex].classList.toggle('slider__item_active');
+//     dots[itemIndex].classList.toggle('slider__dot_active');
+// }
 
-// Смена слайда на следующий
+// // Смена слайда на следующий
 
-const changeImageNext = () => {
-    if (itemIndex < images.length - 1) {
-        itemIndex++;
-    } else {
-        itemIndex = 0;
-    }
+// const changeImageNext = () => {
+//     if (itemIndex < images.length - 1) {
+//         itemIndex++;
+//     } else {
+//         itemIndex = 0;
+//     }
 
-    resetImageClass();
-    images[itemIndex].classList.toggle('slider__item_active');
-    dots[itemIndex].classList.toggle('slider__dot_active');
-}
+//     resetImageClass();
+//     images[itemIndex].classList.toggle('slider__item_active');
+//     dots[itemIndex].classList.toggle('slider__dot_active');
+// }
 
-// Сброс класса активного слайда
+// // Сброс класса активного слайда
+
+// const resetImageClass = () => {
+//     images.forEach(image => {
+//         image.classList.remove('slider__item_active');
+//     });
+
+//     dots.forEach(dot => {
+//         dot.classList.remove('slider__dot_active');
+//     });
+// }
+
+// ВАРИАНТ 2
+
+const findItemIndex = () => {
+	return images.findIndex((image) =>
+		image.classList.contains("slider__item_active")
+	);
+};
+
+arrows.forEach((arrow) => {
+    let itemIndex;
+
+	if (arrow.classList.contains("slider__arrow_prev")) {
+		arrow.onclick = () => {
+            itemIndex = findItemIndex();
+
+			if (itemIndex > 0) {
+				itemIndex--;
+			} else {
+				itemIndex = images.length - 1;
+			}
+
+			showActiveImage(itemIndex);
+		};
+	} else if (arrow.classList.contains("slider__arrow_next")) {
+		arrow.onclick = () => {
+            itemIndex = findItemIndex();
+
+			if (itemIndex < images.length - 1) {
+				itemIndex++;
+			} else {
+				itemIndex = 0;
+			}
+
+			showActiveImage(itemIndex);
+		};
+	}
+});
+
+dots.forEach((dot) => {
+	dot.onclick = () => {
+		dot.classList.toggle("slider__dot_active");
+		const dotIndex = dots.indexOf(dot);
+
+		showActiveImage(dotIndex);
+	};
+});
+
+const showActiveImage = (index) => {
+	resetImageClass();
+	images[index].classList.toggle("slider__item_active");
+	dots[index].classList.toggle("slider__dot_active");
+};
 
 const resetImageClass = () => {
-    images.forEach(image => {
-        image.classList.remove('slider__item_active');
-    });
+	images.forEach((image) => {
+		image.classList.remove("slider__item_active");
+	});
 
-    dots.forEach(dot => {
-        dot.classList.remove('slider__dot_active');
-    });
-}
+	dots.forEach((dot) => {
+		dot.classList.remove("slider__dot_active");
+	});
+};
