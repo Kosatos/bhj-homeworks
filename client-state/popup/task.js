f7f5dfd321a431a49f0a8dc2e803fd65ce626afc
Konstantin Muzyka <kosatkinms@mail.ru>
2022-01-04 15:42:12 +0300

    refactoring popup

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,7 +1,7 @@
 const modal = document.getElementById("subscribe-modal");
 const closeButton = document.querySelector(".modal__close_times");
 
-if(!document.cookie.includes("true")) {
+if(!document.cookie.includes("close=true")) {
     modal.classList.add('modal_active');
 }
 
